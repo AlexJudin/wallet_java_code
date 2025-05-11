8471d4a02f84783eb7b0cb9bbf4012d7478afb99
AlexJudin <spdante@mail.ru>
2025-05-11 21:19:27 +0300

    Добавлен кэш

--- internal/cache/redis.go
+++ internal/cache/redis.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"context"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -9,11 +10,17 @@ type User struct {
 	Name string `json:"name"`
 }
 
-func ConnectToRedis(connString string) *redis.Client {
-	return redis.NewClient(&redis.Options{
+func ConnectToRedis(connString string) (*redis.Client, error) {
+	client := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "1234",
 		DB:       0,
 	})
 
+	status := client.Ping(context.Background())
+	if err := status.Err(); err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
