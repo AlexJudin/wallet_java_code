98313bdb6fbc6736f8cf6c1963fa18af665c0a18
AlexJudin <spdante@mail.ru>
2025-05-20 23:00:58 +0300

    Реализован Kafka

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
+	"sync"
 	"syscall"
 	"time"
 
+	"github.com/IBM/sarama"
 	"github.com/go-chi/chi/v5"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/AlexJudin/wallet_java_code/config"
 	"github.com/AlexJudin/wallet_java_code/internal/api/controller"
 	"github.com/AlexJudin/wallet_java_code/internal/cache"
+	"github.com/AlexJudin/wallet_java_code/internal/model"
 	"github.com/AlexJudin/wallet_java_code/internal/repository"
 )
 
@@ -48,6 +53,8 @@ func main() {
 
 	r := chi.NewRouter()
 	controller.AddRoutes(cfg, db, redisClient, r)
+
+	startKafka()
 
 	startHTTPServer(cfg, r)
 }
@@ -89,5 +96,88 @@ func startHTTPServer(cfg *config.Сonfig, r *chi.Mux) {
 		log.Info("The server has been stopped successfully")
 	case err = <-serverErr:
 		log.Errorf("Server error: %+v", err)
+	}
+}
+
+const (
+	PaymentOperationTopic = "PaymentOperation"
+)
+
+func startKafka() {
+	consumer, err := sarama.NewConsumer([]string{"localhost:9092"}, nil)
+	if err != nil {
+		log.Fatalf("failed to create consumer: %+v", err)
+	}
+	defer consumer.Close()
+
+	producer, err := sarama.NewSyncProducer([]string{"localhost:9092"}, nil)
+	if err != nil {
+		log.Fatalf("failed to create producer: %+v", err)
+	}
+	defer producer.Close()
+
+	partConsumer, err := consumer.ConsumePartition(PaymentOperationTopic, 0, sarama.OffsetNewest)
+	if err != nil {
+		log.Fatalf("Failed to consume partition: %+v", err)
+	}
+	//defer partConsumer.Close()
+
+	publish(producer)
+
+	var wg sync.WaitGroup
+
+	for w := 1; w <= 3; w++ {
+		wg.Add(1)
+		go Handler(w, partConsumer, &wg)
+	}
+
+	wg.Wait()
+	partConsumer.Close()
+}
+
+func Handler(id int, partConsumer sarama.PartitionConsumer, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		// Чтение сообщения из Kafka
+		case msg, ok := <-partConsumer.Messages():
+			if !ok {
+				log.Println("Channel closed, exiting goroutine")
+				return
+			}
+			// Десериализация входящего сообщения из JSON
+			var receivedMessage model.PaymentOperation
+			err := json.Unmarshal(msg.Value, &receivedMessage)
+
+			if err != nil {
+				log.Printf("Error unmarshaling JSON: %+v", err)
+				continue
+			}
+			log.Println("worker", id, "started job", receivedMessage.WalletId)
+			time.Sleep(5 * time.Second)
+			log.Println("worker", id, "finished job", receivedMessage.WalletId)
+		}
+	}
+}
+
+func publish(producer sarama.SyncProducer) {
+	for i := 1; i <= 15; i++ {
+		msg := model.PaymentOperation{
+			WalletId:      strconv.Itoa(i),
+			OperationType: model.Deposit,
+			Amount:        3000 + int64(i),
+		}
+
+		res, _ := json.Marshal(msg)
+		resp := &sarama.ProducerMessage{
+			Topic: PaymentOperationTopic,
+			Key:   sarama.StringEncoder(strconv.Itoa(i)),
+			Value: sarama.StringEncoder(res),
+		}
+
+		_, _, err := producer.SendMessage(resp)
+		if err != nil {
+			log.Printf("Failed to produce message: %+v", err)
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.0
 toolchain go1.23.7
 
 require (
+	github.com/IBM/sarama v1.45.1
 	github.com/go-chi/chi/v5 v5.2.1
 	github.com/go-chi/httprate v0.14.1
 	github.com/golang-jwt/jwt/v4 v4.5.2
@@ -22,26 +23,41 @@ require (
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/eapache/go-resiliency v1.7.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
 	github.com/go-openapi/spec v0.20.4 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.7.6 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/pierrec/lz4/v4 v4.1.22 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
-	golang.org/x/net v0.30.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 	golang.org/x/tools v0.26.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
